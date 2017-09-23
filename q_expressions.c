cf1466bd853e63f77d1bb953c6ae09210883418f
Francisco Azevedo Alves <aaalves.francisco@gmail.com>
2017-09-23 15:53:47 -0300

    Fix lval_read

--- q_expressions.c
+++ q_expressions.c
@@ -40,7 +40,7 @@ typedef struct lval {
 } lval;
 
 /* Create an enumeration of possible lval types */
-enum { LVAL_ERR, LVAL_NUM, LVAL_SYM, LVAL_SEXPR };
+enum { LVAL_ERR, LVAL_NUM, LVAL_SYM, LVAL_SEXPR, LVAL_QEXPR };
 
 /* Construct a pointer to a new number lval */
 lval* lval_num(long x) {
@@ -77,6 +77,15 @@ lval* lval_sexpr(void) {
   return v;
 }
 
+/* Construct a pointer to a new Qexpr type lval */
+lval* lval_qexpr(void) {
+  lval* v = malloc(sizeof(lval));
+  v->type = LVAL_QEXPR;
+  v->count = 0;
+  v->cell = NULL;
+  return v;
+}
+
 void lval_del(lval* v) {
 
   switch (v->type) {
@@ -87,8 +96,9 @@ void lval_del(lval* v) {
   case LVAL_ERR: free(v->err); break;
   case LVAL_SYM: free(v->sym); break;
 
-    /* If Sexpr then delete all elements inside */
+    /* If Sexpr or Qexpr then delete all elements inside */
   case LVAL_SEXPR:
+  case LVAL_QEXPR:
     for (int i = 0; i < v->count; i++) {
       lval_del(v->cell[i]);
     }
@@ -126,12 +136,15 @@ lval* lval_read(mpc_ast_t* t) {
   /* If root (>) or sexpr then create an empty list */
   lval* x = NULL;
   if (strcmp(t->tag, ">") == 0) { x = lval_sexpr(); }
-  if (strcmp(t->tag, "sexpr"))  { x = lval_sexpr(); }
+  if (strstr(t->tag, "sexpr"))  { x = lval_sexpr(); }
+  if (strstr(t->tag, "qexpr"))  { x = lval_qexpr(); }
 
   /* Fill this list with any valid expression contained within */
   for (int i = 0; i < t->children_num; i++) {
     if (strcmp(t->children[i]->contents, "(") == 0) { continue; }
     if (strcmp(t->children[i]->contents, ")") == 0) { continue; }
+    if (strcmp(t->children[i]->contents, "{") == 0) { continue; }
+    if (strcmp(t->children[i]->contents, "}") == 0) { continue; }
     if (strcmp(t->children[i]->tag,  "regex") == 0) { continue; }
     x = lval_add(x, lval_read(t->children[i]));
   }
@@ -159,10 +172,11 @@ void lval_expr_print(lval* v, char open, char close) {
 /* Print an "lval" */
 void lval_print(lval* v) {
   switch (v->type) {
-    case LVAL_NUM: printf("%li", v->num); break;
-    case LVAL_ERR: printf("Error: %s", v->err); break;
-    case LVAL_SYM: printf("%s", v->sym); break;
+    case LVAL_NUM:   printf("%li", v->num); break;
+    case LVAL_ERR:   printf("Error: %s", v->err); break;
+    case LVAL_SYM:   printf("%s", v->sym); break;
     case LVAL_SEXPR: lval_expr_print(v, '(', ')'); break;
+    case LVAL_QEXPR: lval_expr_print(v, '{', '}'); break;
   }
 }
 
@@ -302,6 +316,7 @@ int main(int argc, char* argv[]) {
   mpc_parser_t* Number = mpc_new("number");
   mpc_parser_t* Symbol = mpc_new("symbol");
   mpc_parser_t* Sexpr = mpc_new("sexpr");
+  mpc_parser_t* Qexpr = mpc_new("qexpr");
   mpc_parser_t* Expr = mpc_new("expr");
   mpc_parser_t* Lispy = mpc_new("lispy");
 
@@ -309,13 +324,14 @@ int main(int argc, char* argv[]) {
   mpca_lang(MPCA_LANG_DEFAULT,
 	"                                                             \
          number   : /-?[0-9]+/ ;                                      \
-         symbol   : '+' | '-' | '*' | '/' | '%' | '^' |               \
-                    \"min\" | \"max\";				      \
+         symbol   : '+' | '-' | '*' | '/' | '%' | '^'                 \
+                  | \"min\" | \"max\";				      \
          sexpr    : '(' <expr>* ')' ;                                 \
-         expr     : <number> | <symbol> | <sexpr> ;        	      \
+         qexpr    : '{' <expr>* '}' ;                                 \
+         expr     : <number> | <symbol> | <sexpr> | <qexpr>;        	      \
          lispy    : /^/ <expr>*  /$/ ;				      \
         ",
-	    Number, Symbol, Sexpr, Expr, Lispy);
+	    Number, Symbol, Sexpr, Qexpr, Expr, Lispy);
   
   puts("fLisp Version 0.0.0.0.1");
   puts("Copyright ©frazeal 2017");
@@ -336,6 +352,7 @@ int main(int argc, char* argv[]) {
       lval* x = lval_eval(lval_read(r.output));
       lval_println(x);
       lval_del(x);
+      mpc_ast_delete(r.output);
     } else {
       mpc_err_print(r.error);
       mpc_err_delete(r.error);
@@ -346,7 +363,7 @@ int main(int argc, char* argv[]) {
     
   }
 
-  mpc_cleanup(5, Number, Symbol, Sexpr, Expr, Lispy);
+  mpc_cleanup(6, Number, Symbol, Sexpr, Qexpr, Expr, Lispy);
   
   return 0;
 
